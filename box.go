f4408de161ab13a069bd7881d8171ca22b404694
Alex Al <alexsputnik@gmail.com>
2022-05-30 21:56:16 +0300

    the second one

--- box.go
+++ box.go
@@ -9,6 +9,7 @@ var (
 	errorOutOfCapacity = errors.New("out of the shapesCapacity range")
 	errorOutOfRange    = errors.New("index is out of range")
 	errorNotExist      = errors.New("shape not exist in the list")
+	inputErrorString   = "input error: %w" // for quality test
 )
 
 // box contains list of shapes and able to perform operations on them
@@ -28,7 +29,7 @@ func NewBox(shapesCapacity int) *box {
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
 	if len(b.shapes) == b.shapesCapacity {
-		return fmt.Errorf("input error: %w", errorOutOfCapacity)
+		return fmt.Errorf(inputErrorString, errorOutOfCapacity)
 	}
 	b.shapes = append(b.shapes, shape)
 	return nil
@@ -38,7 +39,7 @@ func (b *box) AddShape(shape Shape) error {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
 	if len(b.shapes) <= i {
-		return nil, fmt.Errorf("input error: %w", errorOutOfRange)
+		return nil, fmt.Errorf(inputErrorString, errorOutOfRange)
 	}
 	return b.shapes[i], nil
 
@@ -48,7 +49,7 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
 	if len(b.shapes) <= i {
-		return nil, fmt.Errorf("input error: %w", errorOutOfRange)
+		return nil, fmt.Errorf(inputErrorString, errorOutOfRange)
 	}
 	RemovedShape := b.shapes[i]
 	b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
@@ -60,7 +61,7 @@ func (b *box) ExtractByIndex(i int) (Shape, error) {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
 	if len(b.shapes) <= i {
-		return nil, fmt.Errorf("input error: %w", errorOutOfRange)
+		return nil, fmt.Errorf(inputErrorString, errorOutOfRange)
 	}
 	RemovedShape := b.shapes[i]
 	b.shapes[i] = shape
@@ -75,7 +76,6 @@ func (b *box) SumPerimeter() float64 {
 		sum += v.CalcPerimeter()
 	}
 	return sum
-	//panic("implement me")
 }
 
 // SumArea provides sum area of all shapes in the list.
@@ -85,21 +85,22 @@ func (b *box) SumArea() float64 {
 		sum += v.CalcArea()
 	}
 	return sum
-	//panic("implement me")
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
 	var counterSlice []int
+	var counter int
 	for i, v := range b.shapes {
 		switch v.(type) {
 		case Circle:
 			counterSlice = append(counterSlice, i)
+			counter++
 		}
 	}
-	if len(counterSlice) == 0 {
-		return fmt.Errorf("input error: %w", errorNotExist)
+	if counter == 0 {
+		return fmt.Errorf(inputErrorString, errorNotExist)
 	}
 	for _, v := range counterSlice {
 		_, err := b.ExtractByIndex(v)
